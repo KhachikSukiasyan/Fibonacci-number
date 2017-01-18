9346795a9ab545bc7c2dd30a8b8fee6dfe987d41
Khachik Sukiasyan <khachatur125@gmail.com>
2017-01-18 23:47:00 +0400

    Initial commit

---
+++ FibonacciNumber/FibonacciNumber/FibNumber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumber
+{
+    public class FibNumber
+    {
+
+        public int this[int k]
+        {
+           get
+           {
+                if (k < 0)
+                {
+                    Console.WriteLine("You can't pass negative index");
+                    return int.MinValue;
+                }
+                else
+                    return function(k);
+           }
+        }
+
+       private int function(int k)
+        {
+            if (k == 0 || k == 1)
+                return 1;
+            if (k == 2)
+                return 2;
+
+            int k1 = 1;
+            int k2 = 1;
+            int k3 = 2;
+
+            for (int i = 0; i < k - 2; i++)
+            {
+                k1 = k2;
+                k2 = k3;
+                k3 = k1 + k2;  
+            }
+
+            return k3;
+        }
+
+    }
+}
---
+++ FibonacciNumber/FibonacciTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FibonacciNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibNumber f = new FibNumber();
+            Console.WriteLine(f[7]);
+            Console.ReadKey();
+        }
+    }
+}
